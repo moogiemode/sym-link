a7555336abf02d50ca93b449ed8db80a44b06c8c
Edwin Ekpenyong <moogiemode@gmail.com>
2024-12-31 15:52:11 -0800

    Updated: src/components/App.tsx src/components/linkedFilesDisplay/LinkedFileInfo.tsx src/components/linkedFilesDisplay/LinkedFilesDisplay.tsx src/store.ts

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,20 +1,11 @@
-import { useState } from 'react';
 import { CreateFileLinkButton } from './createFileLink/CreateFileLinkButton';
-// import { LinkedFilesDisplay } from './linkedFilesDisplay/LinkedFilesDisplay';
+import { LinkedFilesDisplay } from './linkedFilesDisplay/LinkedFilesDisplay';
 
 export const App = () => {
-  const [titleState, setTitleState] = useState('this is my new title');
-
-  const openDialog = async () => {
-    const path = await window.electronAPI.openDialog();
-    console.log(path);
-  };
-
   return (
     <>
-      <button onClick={async () => await window.electronAPI.saveSettings('NEW APPLE', 'OLD BANANA')}>Open Dialog</button>
       <CreateFileLinkButton />
-      {/* <LinkedFilesDisplay /> */}
+      <LinkedFilesDisplay />
     </>
   );
 };
--- src/components/linkedFilesDisplay/LinkedFileInfo.tsx
+++ src/components/linkedFilesDisplay/LinkedFileInfo.tsx
@@ -1,31 +1,31 @@
-// import { ArrowRightIcon } from '@/icons/ArrowRightIcon';
-// import { LinkedFolder } from '@/types';
-// import { FC } from 'react';
+import { ArrowRightIcon } from '@/icons/ArrowRightIcon';
+import { LinkedFolder } from '@/types';
+import { FC } from 'react';
 
-// export const LinkedFileInfo: FC<{ linkedFile: LinkedFolder | string }> = ({ linkedFile }) => {
-//   const { dirKey, sourceDirName, outputDirName, sourceDirPath, outputDirPath, filesSynced, filesMissingFromSource, filesMissingFromOutput, filesMissingFromBoth, filesInOutputNoLongerSymLinks, timeSynced } =
-//     typeof linkedFile === 'string' ? {} : linkedFile;
-//   return (
-//     <div className="flex items-center gap-3">
-//       <div className="avatar size-14 bg-success mask mask-squircle">
-//         <div>
-//           <div className="text-xl font-bold flex justify-center items-center h-full w-full">A➤E</div>
-//         </div>
-//       </div>
-//       <div>
-//         <div className="flex items-center gap-3">
-//           <div className="font-bold">{sourceDirName}</div>
-//           <ArrowRightIcon className="size-6" />
-//           <div className="font-bold">{outputDirName}</div>
-//         </div>
-//         <div className="text-sm opacity-50">{`${sourceDirPath} → ${outputDirPath}`}</div>
-//         <div>
-//           <div className="flex items-end">
-//             <div className="text-sm opacity-75">Last Synced On: </div>
-//             <div className="text-xs opacity-50">{outputDirPath}</div>
-//           </div>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// };
+export const LinkedFileInfo: FC<{ linkedFile: LinkedFolder | string }> = ({ linkedFile }) => {
+  const { dirKey, sourceDirName, outputDirName, sourceDirPath, outputDirPath, filesSynced, filesMissingFromSource, filesMissingFromOutput, filesMissingFromBoth, filesInOutputNoLongerSymLinks, timeSynced } =
+    typeof linkedFile === 'string' ? {} : linkedFile;
+  return (
+    <div className="flex items-center gap-3">
+      <div className="avatar size-14 bg-success mask mask-squircle">
+        <div>
+          <div className="text-xl font-bold flex justify-center items-center h-full w-full">A➤E</div>
+        </div>
+      </div>
+      <div>
+        <div className="flex items-center gap-3">
+          <div className="font-bold">{sourceDirName}</div>
+          <ArrowRightIcon className="size-6" />
+          <div className="font-bold">{outputDirName}</div>
+        </div>
+        <div className="text-sm opacity-50">{`${sourceDirPath} → ${outputDirPath}`}</div>
+        <div>
+          <div className="flex items-end">
+            <div className="text-sm opacity-75">Last Synced On: </div>
+            <div className="text-xs opacity-50">{outputDirPath}</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
