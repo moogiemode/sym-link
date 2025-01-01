e1296ec2d0f8eb888562808546d229e0cded116a
Edwin Ekpenyong <moogiemode@gmail.com>
2025-01-01 11:32:34 -0800

    Updated: src/components/linkedFilesDisplay/LinkedFileInfo.tsx

--- src/components/linkedFilesDisplay/LinkedFileInfo.tsx
+++ src/components/linkedFilesDisplay/LinkedFileInfo.tsx
@@ -20,6 +20,9 @@ export const LinkedFileInfo: FC<{ linkedFile: LinkedFolder | string }> = ({ link
     timeSynced,
   } = typeof linkedFile === 'string' ? {} : linkedFile;
 
+  console.log(linkedFile);
+
+  const syncStatus = filesSynced.length === linkedFiles.length ? {className: 'text-success', text: 'All Good'} : filesSynced.length === 0 ? {className: 'text-error', text: 'All Synced Files are Missing'} : {className: 'text-warning', text: 'Some Files are Missing'};
   return (
     <div className="flex items-center bg-base-200 gap-3 p-2 rounded-md">
       <div className="avatar size-14 bg-base-300 mask mask-squircle">
@@ -42,9 +45,9 @@ export const LinkedFileInfo: FC<{ linkedFile: LinkedFolder | string }> = ({ link
           </div>
         </div>
         <div className="text-sm opacity-75">{`${sourceDirPath} → ${outputDirPath}`}</div>
-        <div className="flex items-end justify-between gap-3 items-center">
+        <div className="flex justify-between gap-3 items-center">
           <div className="text-sm opacity-40">Last Synced: {new Date(timeSynced).toLocaleString()}</div>
-          <div className="text-xs opacity-50 text-success">Sync Status: All Good</div>
+          <div className={`text-xs opacity-50 ${syncStatus.className}`}>Sync Status: {syncStatus.text}</div>
         </div>
       </div>
     </div>
