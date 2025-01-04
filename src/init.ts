408156870819ae1f03927b9ca54f85ceb9735627
Edwin Ekpenyong <moogiemode@gmail.com>
2025-01-04 15:20:08 -0800

    Update init.ts

--- src/init.ts
+++ src/init.ts
@@ -2,16 +2,22 @@ import { getAllLinkedFoldersFromSettings } from './settings';
 import { useSymLinkStore } from './store';
 import { LinkedFolder } from './types';
 
-const linkedFolders = await getAllLinkedFoldersFromSettings().then(folders => {
-  // if (!folders) return folders;
-  return folders.reduce(
-    (acc, folder) => {
-      acc[folder.dirKey] = folder;
-      return acc;
-    },
-    {} as Record<string, LinkedFolder>,
-  );
-});
-if (linkedFolders) {
-  useSymLinkStore.getState().setAllLinkedFiles(linkedFolders);
-}
+const init = async () => {
+  await getAllLinkedFoldersFromSettings()
+    .then(folders => {
+      // if (!folders) return folders;
+      return folders.reduce(
+        (acc, folder) => {
+          acc[folder.dirKey] = folder;
+          return acc;
+        },
+        {} as Record<string, LinkedFolder>,
+      );
+    })
+    .then(linkedFolders => {
+      if (!linkedFolders) return;
+      useSymLinkStore.getState().setAllLinkedFiles(linkedFolders);
+    });
+};
+
+init();
